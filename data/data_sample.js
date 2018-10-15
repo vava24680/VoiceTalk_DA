50d89cefe9f5fa8949ecfa6a0a8d9a3a9315862b
Yu-Hao Chang <9203700@gmail.com>
2018-10-15 12:07:35 +0800

    Add crawler for device types and traits

--- data/data_sample.js
+++ data/data_sample.js
@@ -1,6 +1,11 @@
+const randomstring = require('randomstring');
+const cheerio = require('cheerio');
+const request = require('request');
+const Q = require('q');
 const labDevices = [];
 const config = require('../configuration/configuration.js');
-const randomstring = require('randomstring');
+const deviceTypesURL = 'https://developers.google.com/actions/smarthome/guides/';
+const deviceTraitsURL = 'https://developers.google.com/actions/smarthome/traits/';
 
 /*
  * Authorization Data.
@@ -8,6 +13,16 @@ const randomstring = require('randomstring');
  * Not support new users login right now
  */
 let authorizationData = {};
+/*
+ * Array stores all device types
+ * crawled from Google smarthome guide
+ */
+let deviceTypesArray = [];
+/*
+ * Array stores all device traits
+ * crawled from Google smarthome guide
+ */
+let deviceTraitsArray = [];
 /*
  * Structure of user
  * uid: {
@@ -59,6 +74,146 @@ function getDevices() {
   return labDevices;
 }
 
+/**
+ * use `request` to crawl the pages containing
+ * all device types and use `cheerio` to get desired
+ * data and then save them to promise object
+ * @return {promise} promise object with an array
+ *     containing all device types
+ */
+function _crawlTypes() {
+  let qObject = Q.defer();
+  let options = {
+    url: deviceTypesURL,
+    method: 'GET',
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      qObject.reject("failed");
+    } else {
+      let $ = cheerio.load(body);
+      let trArray = $('table tr');
+      let deviceTypesArray = [];
+      for (let i = 1; i < trArray.length; ++i) {
+        // First way to crawl each type
+        deviceTypesArray.push(trArray.eq(i).attr('id'));
+        // Second way to crawl each type
+        /*
+         * let tdArray = trArray.eq(i).find('td);
+         * deviceTypesArray.push(tdArray.eq(1).text());
+         */
+      }
+    }
+  });
+  return qObject.promise;
+}
+
+/**
+ * set the value of variable `deviceTypesArray`
+ * @param {Array} an array contains all device types
+ * @return {void}
+ */
+function _setDeviceTypesArray(typesArray) {
+  deviceTypesArray = typesArray;
+}
+
+/**
+ * return an array with all device types
+ * @return {Array} an array contains all device types
+ */
+function getDeviceTypesArray(typesArray) {
+  return deviceTypesArray;
+}
+
+/**
+ * crawl all device types by calling
+ * function `_crawlTypes` and
+ * save them into an array by using
+ * function `_setDeviceTypesArray`
+ * @return {void}
+ */
+function crawlTypes() {
+  _crawlTypes()
+      .done(
+        (typesArray) => {
+          _setDeviceTypesArray(typesArray);
+        },
+        (failMessage) => {
+          console.log(failMessage);
+        }
+      );
+}
+
+/**
+ * use `request` to crawl the pages containing
+ * all device traits and use `cheerio` to get desired
+ * data and then save them to promise object
+ * @return {promise} promise object with an array
+ *     containing all device traits
+ */
+function _crawlTraits() {
+  let qObject = Q.defer();
+  let options = {
+    url: deviceTraitsURL,
+    method: 'GET',
+  };
+  request(options, (err, response, body) => {
+    if (err) {
+      qObject.reject("failed");
+    } else {
+      let $ = cheerio.load(body);
+      let trArray = $('table tr');
+      let deviceTraitsArray = [];
+      for (let i = 1; i < trArray.length; ++i) {
+        // First way to crawl each trait
+        deviceTraitsArray.push(trArray.eq(i).attr('id'));
+        // Second way to crawl each trait
+        /*
+         * let tdArray = trArray.eq(i).find('td);
+         * deviceTraitsArray.push(tdArray.eq(1).text());
+         */
+      }
+    }
+  });
+  return qObject.promise;
+}
+
+/**
+ * set the value of variable `deviceTraitsArray`
+ * @param {Array} an array contains all device traits
+ * @return {void}
+ */
+function _setDeviceTraitsArray(traitsArray) {
+  deviceTraitsArray = traitsArray;
+}
+
+/**
+ * return an array with all device traits
+ * @return {Array} an array contains all device traits
+ */
+function getDeviceTraitsArray() {
+  return deviceTraitsArray;
+}
+
+/**
+ * crawl all device traits by calling
+ * function `_crawlTraits` and
+ * save them into an array by using
+ * function `_setDeviceTraitsArray`
+ * @return {void}
+ */
+function crawlTraits() {
+  _crawlTraits()
+      .done(
+        (traitsArray) => {
+          _setDeviceTraitsArray(traitsArray);
+        },
+        (failMessage) => {
+          console.log(failMessage);
+        }
+      );
+}
+
 /*
  * Structure of labDevices
  * [
@@ -86,3 +241,7 @@ function getDevices() {
 module.exports.labDevices = labDevices;
 module.exports.authorizationData = authorizationData;
 module.exports.getDevices = getDevices;
+module.exports.crawlTypes = crawlTypes;
+module.exports.crawlTraits = crawlTraits;
+module.exports.getDeviceTypesArray = getDeviceTypesArray;
+module.exports.getDeviceTraitsArray = getDeviceTraitsArray;
